2d5204cccb7bd05977ed413842b23b86150f2efd
glebbykov <122178014+glebbykov@users.noreply.github.com>
2023-01-11 05:01:19 +0300

    Update bubble_sort.sh
    The script now includes the ability to read values from a file, as well as to verify the contents of the file

--- bubble_sort.sh
+++ bubble_sort.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 
-# Bubble sort function
+file=$1
+
+# check if file exists
+if [ ! -f $file ]; then
+  echo "Error: file not found"
+  exit 1
+fi
+
+# read file into an array
+IFS=$'\n' read -d '' -r -a numbers < $file
+
+# check if file contains non-numeric elements
+for i in "${numbers[@]}"; do
+  if ! [[ $i =~ ^[0-9]+$ ]]; then
+    echo "Error: file contains non-numeric elements"
+    exit 1
+  fi
+done
+
+# bubble sort function
 bubble_sort() {
-    local -a arr=("$@")
-    # Сonsecutive comparison of adjacent array elements
-    for ((i = 0; i < ${#arr[@]}; i++)); do
-        for ((j = 0; j < ${#arr[@]}-1; j++)); do
-            if [[ ${arr[j]} -gt ${arr[j+1]} ]]; then
-                # Swap items
-                temp=${arr[j]}
-                arr[j]=${arr[j+1]}
-                arr[j+1]=$temp
-            fi
-        done
+  local -n arr=$1
+
+  for ((i = 0; i < ${#arr[@]}; i++)); do
+    for ((j = 0; j < ${#arr[@]}-1; j++)); do
+      if (( ${arr[j]} > ${arr[j+1]} )); then
+        temp=${arr[j]}
+        arr[j]=${arr[j+1]}
+        arr[j+1]=$temp
+      fi
     done
-    echo "${arr[*]}"
+  done
+  echo "${arr[*]}"
 }
 
-bubble_sort "$@"
+bubble_sort numbers
